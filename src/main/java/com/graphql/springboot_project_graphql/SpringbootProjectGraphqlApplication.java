e8d3cdc7eaa31018b5c2032b3c308bd4ba027a80
Johan <johannedpro@gmail.com>
2026-02-17 15:02:39 +0100

    init spring boot + graphql + hibernate

---
+++ src/main/java/com/graphql/springboot_project_graphql/SpringbootProjectGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.graphql.springboot_project_graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProjectGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProjectGraphqlApplication.class, args);
+	}
+
+}
